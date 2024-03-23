ef9c25d7858b127bd4165a1d54dcebe0520d801a
hwanghaein <hhi9037@naver.com>
2024-03-23 14:18:38 +0900

    CORS 설치

--- fetch/server/app.js
+++ fetch/server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -29,6 +31,6 @@ app.post("/api/todo", (req, res) => {
   return res.send('sucess')
 });
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("server start!!");
 });
